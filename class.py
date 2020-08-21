a01ab859c3429cfd9ca90738f8b6ffc000cca433
Abhiman 21 <bachhil.abhishek@gmail.com>
2020-08-22 02:34:23 +0530

    classes and objects

---
+++ class.py
@@ -0,0 +1,13 @@
+"""
+A simple calss without muich blah blah defining a variable and a function
+"""
+
+class no_blah_blah:
+    i=123
+    def abc():
+        str="go to heaven hell is full"
+  
+
+x=no_blah_blah()
+print (x.i)
+ 
